3bab07aad66970bceaf68ce33a362d8c9f6a1534
Matías Salinas <msalinas61192@gmail.com>
2019-01-18 08:16:12 -0300

    main2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "AKIAIJKACWAQOSDLS7RQ"
+  secret_key = "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"
   region     = "us-east-1"
 }
