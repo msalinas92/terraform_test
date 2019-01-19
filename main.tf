149b77ca8a32130e0dcc3fb237e60e7da317329a
Matías Salinas <msalinas61192@gmail.com>
2019-01-19 09:02:48 -0300

    main4

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 
+
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+
 provider "aws" {
   access_key = "${var.AWS_ACCESS_KEY_ID}"
   secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-  region     = "${var.AWS_DEFAULT_REGION}"
+  region     = "us-east-1"
 }
