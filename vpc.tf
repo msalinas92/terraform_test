855221a86363be24273bf3364cc0a0ec4ddeabb4
Matías Salinas <msalinas61192@gmail.com>
2019-01-19 09:33:24 -0300

    subnet-vpc

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,13 @@ resource "aws_vpc" "bizware_vpc" {
     Name = "tf-example-bizware"
   }
 }
+
+resource "aws_subnet" "bizware_subnet" {
+  vpc_id            = "${aws_vpc.bizware_vpc.id}"
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "tf-example-bizware"
+  }
+}
